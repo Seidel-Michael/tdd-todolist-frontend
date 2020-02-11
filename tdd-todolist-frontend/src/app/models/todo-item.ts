2feb9968db8ee5950a391b378d5013a49dc3de0f
Fabian Beyerlein <fabian.beyerlein@balluff.de>
2020-02-11 12:04:51 +0100

    chore: Remove commented out code

--- tdd-todolist-frontend/src/app/models/todo-item.ts
+++ tdd-todolist-frontend/src/app/models/todo-item.ts
@@ -1,13 +1,3 @@
-// export class TodoItem {
-//   id: string;
-//   title: string;
-//   state: boolean;
-
-//   constructor(values: object = {}) {
-//     Object.assign(this, values);
-//   }
-// }
-
 export interface TodoItem {
   id?: string;
   title: string;
